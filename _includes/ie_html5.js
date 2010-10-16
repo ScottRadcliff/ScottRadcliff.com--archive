7a04b0ce8931f1c55b01b9c99d84d3da80882e20
Scott Radcliff <radcliffsc@gmail.com>
2010-10-16 13:19:22 -0400

    Initial Commit

---
+++ _includes/ie_html5.js
@@ -0,0 +1,7 @@
+<script type="text/javascript"> 
+	document.createElement('header');
+	document.createElement('footer');
+	document.createElement('nav');
+	document.createElement('aside');
+	document.createElement('section');
+</script>
